63d5f574d8ec047c7dbd36e781e6b69e00a5d728
MansurSambiev <mahcypc@mail.ru>
2026-01-06 19:26:18 +0300

    added migrations

--- src/main.py
+++ src/main.py
@@ -3,10 +3,10 @@ from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
 import sys
 from pathlib import Path
+from src.api.hotels import router as router_hotels
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from src.api.hotels import router as router_hotels
 
 app = FastAPI(docs_url=None)
 
