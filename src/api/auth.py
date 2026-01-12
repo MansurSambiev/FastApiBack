51c17afc9c37ab01c43d20bfca8173c45bd0db68
MansurSambiev <mahcypc@mail.ru>
2026-01-12 23:50:26 +0300

    reformat users table, added jwt token generation

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,8 @@
-from fastapi import APIRouter, Body
+from datetime import datetime, timezone, timedelta
+
+from fastapi import APIRouter, Body, HTTPException
 from passlib.context import CryptContext
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.database import async_session_maker
@@ -9,6 +12,23 @@ router = APIRouter(prefix='/auth', tags=['Авторизация и аутент
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = 'HS256'
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {'exp': expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 
 @router.post('/register')
 async def register_user(
@@ -16,20 +36,15 @@ async def register_user(
             '1': {'summary': 'User', 'value': {
                 'email': 'example@email.com',
                 'password': 'p@s$w0rd',
-                'first_name': 'Vasya',
-                'last_name': 'Pupkin'
             }}
         })
 
 ):
-
     hashed_password = pwd_context.hash(data.password)
 
     new_user_data = UserAdd(
         email=data.email,
         hashed_password=hashed_password,
-        first_name=data.first_name,
-        last_name=data.last_name
     )
 
     async with async_session_maker() as session:
@@ -37,3 +52,17 @@ async def register_user(
         await session.commit()
 
     return {'status': 'OK'}
+
+
+@router.post('/login')
+async def login_user(
+        data: UserRequestAdd,
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail='Пользователь с таким email не зарегистрирован')
+        if not verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail='Пароль неверный')
+        access_token = create_access_token({'user_id': user.id})
+        return {'access_token': access_token}
