60180c80f1d2ecaf1b78987b7332cbbaedf95323
MansurSambiev <mahcypc@mail.ru>
2026-01-13 00:24:41 +0300

    refactor

--- src/config.py
+++ src/config.py
@@ -15,6 +15,10 @@ class Settings(BaseSettings):
     def DB_URL(self):
         return f'postgresql+psycopg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
     )
