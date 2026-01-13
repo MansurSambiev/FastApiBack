3acad15d2268188057e1c3d630932a25bc817bd3
MansurSambiev <mahcypc@mail.ru>
2026-01-13 20:56:57 +0300

    added decoding token

--- src/api/auth.py
+++ src/api/auth.py
@@ -43,14 +43,20 @@ async def login_user(
             raise HTTPException(status_code=401, detail='Пользователь с таким email не зарегистрирован')
         if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail='Пароль неверный')
+
         access_token = AuthService().create_access_token({'user_id': user.id})
         response.set_cookie('access_token', access_token)
         return {'access_token': access_token}
 
 
-# @router.get('/only_auth')
-# async def only_auth(
-#         request: Request,
-# ):
-#
-#     access_token = request.cookies.get('access_token', None)
+@router.get('/only_auth')
+async def only_auth(
+        request: Request,
+):
+
+    access_token = request.cookies.get('access_token', None)
+    data = AuthService().decode_token(access_token)
+    user_id = data['user_id']
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(id=user_id)
+        return user
