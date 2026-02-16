4247bda0c13590680abce276ecbf34845a5058af
MansurSambiev <mahcypc@mail.ru>
2026-02-16 22:30:29 +0300

    added pagination and filtration to hotels

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,14 +13,25 @@ class HotelRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
+    async def get_filtered_by_date(
             self,
+            date_from: date,
+            date_to: date,
             location,
             title,
             limit,
             offset
     ) -> list[Hotel]:
-        query = select(HotelsOrm)
+
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotel_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotel_ids_to_get))
+
         if location:
             query = query.filter(func.lower(HotelsOrm.location).contains(location.strip().lower()))
         if title:
@@ -35,16 +46,3 @@ class HotelRepository(BaseRepository):
         return [Hotel.model_validate(hotel,
                                      from_attributes=True) for hotel in result.scalars().all()]
 
-    async def get_filtered_by_date(
-            self,
-            date_from: date,
-            date_to: date,
-    ):
-        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
-        hotel_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-        )
-
-        return await self.get_filtered(HotelsOrm.id.in_(hotel_ids_to_get))
