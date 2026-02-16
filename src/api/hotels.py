4b6ec4779e7e62e16d7fdbc4ea1edc1ee02fb627
MansurSambiev <mahcypc@mail.ru>
2026-02-16 22:03:10 +0300

    update get_hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from fastapi import Query, APIRouter, Body
 
 from src.api.dependencies import PaginationDep, DBDep
@@ -17,14 +19,21 @@ async def get_hotels(
         db: DBDep,
         title: str | None = Query(None, description="Название отеля"),
         location: str | None = Query(None, description='Адрес'),
+        date_from: date = Query(example='2026-02-10'),
+        date_to: date = Query(example='2026-02-15'),
 ):
     per_page = pagination.per_page or 5
 
-    return await db.hotels.get_all(
-        location=location,
-        title=title,
-        limit=per_page,
-        offset=per_page * (pagination.page - 1)
+    # return await db.hotels.get_all(
+    #     location=location,
+    #     title=title,
+    #     limit=per_page,
+    #     offset=per_page * (pagination.page - 1)
+    # )
+
+    return await db.hotels.get_filtered_by_date(
+        date_from=date_from,
+        date_to=date_to,
     )
 
 
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,10 +1,8 @@
 from datetime import date
 
-from sqlalchemy import select, func
-
-from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.rooms import Room
 
 
@@ -18,56 +16,6 @@ class RoomRepository(BaseRepository):
             date_from: date,
             date_to: date,
     ):
-        """
-        with rooms_count as (
-            select room_id, count(*) as rooms_booked from bookings
-            where date_from <= '2026-02-10' and date_to >= '2026-02-15'
-            group_by room_id
-            ),
-        rooms_left_table as (
-            select rooms.id as room.id, quantity - coalesce(rooms_booked, 0) as rooms_left
-            from rooms
-            left join rooms_count on rooms.id = rooms_count.room.id
-            )
-        select * from rooms_left_table
-        where rooms_left > 0 and room_id in (select id from rooms where hotel_id = hotel_id)
-        """
 
-        rooms_count = (
-            select(BookingsOrm.room_id, func.count('*').label('rooms_booked'))
-            .select_from(BookingsOrm)
-            .filter(
-                BookingsOrm.date_from <= date_to,
-                BookingsOrm.date_to >= date_from,
-            )
-            .group_by(BookingsOrm.room_id)
-            .cte(name='rooms_count')
-        )
-
-        rooms_left_table = (
-            select(
-                RoomsOrm.id.label('room_id'),
-                (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0))
-                .label('rooms_left'),
-            )
-            .select_from(RoomsOrm)
-            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
-            .cte(name='rooms_left_table')
-        )
-
-        rooms_ids_for_hotel = (
-            select(RoomsOrm.id)
-            .select_from(RoomsOrm)
-            .filter_by(hotel_id=hotel_id)
-            .subquery(name='rooms_ids_for_hotel')
-        )
-
-        rooms_ids_to_get = (
-            select(rooms_left_table.c.room_id)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0,
-                    rooms_left_table.c.room_id.in_(select(rooms_ids_for_hotel)),
-                    )
-        )
-
+        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id)
         return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
